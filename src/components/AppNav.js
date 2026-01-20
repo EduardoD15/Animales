7795aa51736072a30c2379b3f7f4bffadef2866c
EduardoD15 <eduardodeossa23@gmail.com>
2026-01-19 21:05:57 -0500

    Funcional todo

--- src/components/AppNav.js
+++ src/components/AppNav.js
@@ -14,9 +14,18 @@ class AppNav extends HTMLElement {
         <div class="nav-brand">
           <h1>Mi App</h1>
         </div>
+        
+        <ul class="nav-categories">
+          <li><i class="fa-solid fa-paw"></i> Mamíferos</li>
+          <li><i class="fa-solid fa-crow"></i> Aves</li>
+          <li><i class="fa-solid fa-fish"></i> Peces</li>
+          <li><i class="fa-solid fa-dragon"></i> Reptiles</li>
+          <li><i class="fa-solid fa-frog"></i> Anfibios</li>
+          <li><i class="fa-solid fa-spider"></i> Invertebrados</li>
+          <li><i class="fa-solid fa-mountain-sun"></i> Paisajes</li>
+        </ul>
+
         <ul class="nav-links">
-          <li><a href="#/" data-link>Home</a></li>
-          <li><a href="#/about" data-link>About</a></li>
           <li><a href="#/contact" data-link>Contact</a></li>
         </ul>
       </nav>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,16 +11,16 @@ class HomePage extends HTMLElement {
   render() {
     this.innerHTML = `
       <div class="page home-page">
-        <h2>Bienvenido al Home</h2>
-        <p>Esta es la página principal de nuestra SPA con Vanilla JavaScript</p>
-        <div class="card">
-          <h3>Características</h3>
-          <ul>
-            <li>Router SPA</li>
-            <li>Web Components</li>
-            <li>JavaScript Vanilla</li>
-          </ul>
-        </div>
+          <h2>Bienvenido al Home</h2>
+          <p>Esta es la página principal de nuestra SPA con Vanilla JavaScript</p>
+          <div class="card">
+            <h3>Características</h3>
+            <ul>
+              <li>Router SPA</li>
+              <li>Web Components</li>
+              <li>JavaScript Vanilla</li>
+            </ul>
+          </div>
       </div>
     `;
   }
